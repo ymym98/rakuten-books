07c7613dedf51207841326a6e626781c5f80067b
ymym98 <jimboyumi@gmail.com>
2022-01-19 15:01:21 +0900

    actionsとBookの部品用クラスを作成

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,24 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import { Book } from "@/types/Book";
+import axios from "axios";
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    books: new Array<Book>(),
   },
-  mutations: {
-  },
+  mutations: {},
   actions: {
+    async getBookList(context, payload) {
+      const response = await axios.get(
+        "https://app.rakuten.co.jp/services/api/BooksBook/Search/20170404?format=json&title=%E5%A4%AA%E9%99%BD&booksGenreId=001004008&applicationId=e06e2a5afcf14b52139c1fb6c58e9dbc"
+      );
+      console.dir("response:" + JSON.stringify(response));
+      payload = response.data;
+      context.commit("addBookList", payload);
+    },
   },
-  modules: {
-  }
-})
+  modules: {},
+});
---
+++ src/types/Book.ts
@@ -0,0 +1,157 @@
+/**
+ * Bookの部品用クラス.
+ */
+export class Book {
+  constructor(
+    // 著者
+    private _author: string,
+    // 著者カナ
+    private _authorKana: string,
+    // 商品説明文
+    private _itemCaption: string,
+    // 価格
+    private _itemPrice: number,
+    // 商品URL
+    private _itemUrl: string,
+    // 商品画像(大)
+    private _largeImageUrl: string,
+    // 商品画像(中)
+    private _mediumImageUrl: string,
+    // 出版社
+    private _publisherName: string,
+    // 発売日
+    private _salesDate: string,
+    // サイズ
+    private _size: string,
+    // 商品画像(小)
+    private _smallImageUrl: string,
+    // サブタイトル
+    private _subTitle: string,
+    // サブタイトルカナ
+    private _subTitleKana: string,
+    // タイトル
+    private _title: string,
+    // タイトルカナ
+    private _titleKana: string
+  ) {}
+
+  public get author(): string {
+    return this._author;
+  }
+
+  public set author(author: string) {
+    this._author = author;
+  }
+
+  public get authorKana(): string {
+    return this._authorKana;
+  }
+
+  public set authorKana(authorKana: string) {
+    this._authorKana = authorKana;
+  }
+
+  public get itemCaption(): string {
+    return this._itemCaption;
+  }
+
+  public set itemCaption(itemCaption: string) {
+    this._itemCaption = itemCaption;
+  }
+
+  public get itemPrice(): number {
+    return this._itemPrice;
+  }
+
+  public set itemPrice(itemPrice: number) {
+    this._itemPrice = itemPrice;
+  }
+
+  public get itemUrl(): string {
+    return this._itemUrl;
+  }
+
+  public set itemUrl(itemUrl: string) {
+    this._itemUrl = itemUrl;
+  }
+
+  public get largeImageUrl(): string {
+    return this._largeImageUrl;
+  }
+
+  public set largeImageUrl(largeImageUrl: string) {
+    this._largeImageUrl = largeImageUrl;
+  }
+
+  public get mediumImageUrl(): string {
+    return this._mediumImageUrl;
+  }
+
+  public set mediumImageUrl(mediumImageUrl: string) {
+    this._mediumImageUrl = mediumImageUrl;
+  }
+
+  public get publisherName(): string {
+    return this._publisherName;
+  }
+
+  public set publisherName(publisherName: string) {
+    this._publisherName = publisherName;
+  }
+
+  public get salesDate(): string {
+    return this._salesDate;
+  }
+
+  public set salesDate(salesDate: string) {
+    this._salesDate = salesDate;
+  }
+
+  public get size(): string {
+    return this._size;
+  }
+
+  public set size(size: string) {
+    this._size = size;
+  }
+
+  public get smallImageUrl(): string {
+    return this._smallImageUrl;
+  }
+
+  public set smallImageUrl(smallImageUrl: string) {
+    this._smallImageUrl = smallImageUrl;
+  }
+
+  public get subTitle(): string {
+    return this._subTitle;
+  }
+
+  public set subTitle(subTitle: string) {
+    this._subTitle = subTitle;
+  }
+
+  public get subTitleKana(): string {
+    return this._subTitleKana;
+  }
+
+  public set subTitleKana(subTitleKana: string) {
+    this._subTitleKana = subTitleKana;
+  }
+
+  public get title(): string {
+    return this._title;
+  }
+
+  public set title(title: string) {
+    this._title = title;
+  }
+
+  public get titleKana(): string {
+    return this._titleKana;
+  }
+
+  public set titleKana(titleKana: string) {
+    this._titleKana = titleKana;
+  }
+}
