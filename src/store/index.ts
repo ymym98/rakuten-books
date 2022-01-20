3c761738ab2d657fa0a91b16d9d20356c0d33442
ymym98 <jimboyumi@gmail.com>
2022-01-20 16:04:55 +0900

    APIで取得したデータをstateの配列に格納

--- src/store/index.ts
+++ src/store/index.ts
@@ -7,46 +7,61 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    // 楽天ブックス書籍検索一覧を格納する配列
     books: new Array<Book>(),
   },
   mutations: {
+    /**
+     * stateのbooksにAPIで取得した情報を格納する.
+     * @param state ステート
+     * @param payload 楽天ブックス書籍検索APIのデータ一覧
+     */
     addBookList(state, payload) {
-      console.dir("payload:" + JSON.stringify(payload));
-      for (const book of payload.Items) {
+      // 初期化
+      state.books = new Array<Book>();
+      for (const item of payload.Items) {
         state.books.push(
           new Book(
-            book.author,
-            book.authorKana,
-            book.itemCaption,
-            book.itemPrice,
-            book.itemUrl,
-            book.largeImageUrl,
-            book.mediumImageUrl,
-            book.publisherName,
-            book.salesDate,
-            book.size,
-            book.smallImageUrl,
-            book.subTitle,
-            book.subTitleKana,
-            book.title,
-            book.titleKana
+            item.Item.author,
+            item.Item.authorKana,
+            item.Item.itemCaption,
+            item.Item.itemPrice,
+            item.Item.itemUrl,
+            item.Item.largeImageUrl,
+            item.Item.mediumImageUrl,
+            item.Item.publisherName,
+            item.Item.salesDate,
+            item.Item.size,
+            item.Item.smallImageUrl,
+            item.Item.subTitle,
+            item.Item.subTitleKana,
+            item.Item.title,
+            item.Item.titleKana
           )
         );
       }
-      console.log("books:" + state.books);
     },
   },
   actions: {
+    /**
+     * APIから情報を取得し、mutationを呼び出す.
+     * @param context コンテキスト
+     * @param payload 楽天ブックス書籍検索APIのデータ一覧
+     */
     async getBookList(context, payload) {
       const response = await axios.get(
-        "https://app.rakuten.co.jp/services/api/BooksBook/Search/20170404?format=json&title=%E5%A4%AA%E9%99%BD&booksGenreId=001004008&applicationId=e06e2a5afcf14b52139c1fb6c58e9dbc"
+        "https://app.rakuten.co.jp/services/api/BooksBook/Search/20170404?format=json&title=%E5%A4%AA%E9%99%BD&booksGenreId=001004008&applicationId=1028649662998988122"
       );
-      console.dir("response:" + JSON.stringify(response));
       payload = response.data;
       context.commit("addBookList", payload);
     },
   },
   getters: {
+    /**
+     * stateのbooks情報を返す.
+     * @param state ステート
+     * @returns 全books情報
+     */
     getBookList(state) {
       return state.books;
     },
--- src/views/Books.vue
+++ src/views/Books.vue
@@ -1,12 +1,23 @@
 <template>
-  <div></div>
+  <div>
+    {{ bookList }}
+  </div>
 </template>
 
 <script lang="ts">
+import { Book } from "@/types/Book";
 import { Component, Vue } from "vue-property-decorator";
 
 @Component
-export default class Books extends Vue {}
+export default class Books extends Vue {
+  private bookList = new Array<Book>();
+
+  async created(): Promise<void> {
+    await this.$store.dispatch("getBookList");
+    this.bookList = this.$store.getters.getBookList;
+    console.dir("bookList:" + JSON.stringify(this.bookList));
+  }
+}
 </script>
 
 <style></style>
