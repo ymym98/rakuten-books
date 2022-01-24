27ec0fe2cf9db3fd416f4a3d54fef8a12726f119
ymym98 <jimboyumi@gmail.com>
2022-01-24 11:38:30 +0900

    navを下揃えにし、間隔を均等にする

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,13 @@
 <template>
-  <div class="header"></div>
+  <header>
+    <img class="icon" src="/img/yumi_book.png" />
+    <ul class="nav">
+      <li class="nav_item"><a href="#"> Home</a></li>
+      <li class="nav_item"><a href="#">Service</a></li>
+      <li class="nav_item"><a href="#">About</a></li>
+      <li class="nav_item"><a href="#">Contact</a></li>
+    </ul>
+  </header>
 </template>
 
 <script lang="ts">
@@ -9,10 +17,6 @@ import { Component, Vue } from "vue-property-decorator";
 export default class Header extends Vue {}
 </script>
 
-<style scoped>
-.header {
-  width: 1440px;
-  height: 127px;
-  background-color: #f3f1e4;
-}
+<style>
+@import "../../public/css/header.css";
 </style>
