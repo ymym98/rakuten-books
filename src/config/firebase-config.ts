31cffeaf8324e4e932b165c80cdd571720d5e641
ymym98 <jimboyumi@gmail.com>
2022-02-02 16:29:07 +0900

    firebaseのauthを実装

--- src/config/firebase-config.ts
+++ src/config/firebase-config.ts
@@ -1,24 +1,11 @@
-import { getStorage } from "firebase/storage";
-import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
-import firebase from "firebase/compat/app";
+import { initializeApp } from "firebase/app";
 
-export const firebaseConfig = {
-  apiKey: "process.env.VUE_APP_API_KEY",
-  authDomain: "process.env.VUE_APP_AUTH_DOMAIN",
-  projectId: "process.env.VUE_APP_PROJECT_ID",
-  storageBucket: "process.env.VUE_APP_STORAGE_BUCKET",
-  messagingSenderId: "process.env.VUE_APP_MESSAGING_SENDING_ID",
-  appId: "process.env.VUE_APP_APP_ID",
+const firebaseConfig = {
+  apiKey: "AIzaSyCt_9gv40KuIaPEoguSn5471vfGA9bzo6s",
+  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+  projectId: process.env.VUE_APP_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDING_ID,
+  appId: process.env.VUE_APP_APP_ID,
 };
-
-console.log(firebaseConfig);
-
-//Firebase.initializeApp()を何度も呼び出すことを防ぐために以下のコードで対応
-export const firebaseApp = !firebase.apps.length
-  ? firebase.initializeApp(firebaseConfig)
-  : firebase.app();
-
-export const storage = getStorage(firebaseApp);
-export const db = getFirestore();
-export const auth = getAuth();
+export const app = initializeApp(firebaseConfig);
--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -15,11 +15,11 @@
 </template>
 
 <script lang="ts">
-import firebase from "firebase/compat/app";
-import "firebase/compat/auth";
-import { firebaseApp, firebaseConfig } from "@/config/firebase-config";
+// import "firebase/compat/auth";
 import { Component, Vue } from "vue-property-decorator";
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+// import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { app } from "../config/firebase-config";
 
 @Component
 export default class Signup extends Vue {
@@ -30,17 +30,40 @@ export default class Signup extends Vue {
   // 登録のエラーメッセージ
   private errorSignup = "";
 
-  async signup(): Promise<void> {
-    try {
-      const auth = getAuth(firebaseApp);
-      createUserWithEmailAndPassword(auth, this.email, this.password);
-      alert("成功！");
+  signup(): void {
+    const auth = getAuth(app);
+    createUserWithEmailAndPassword(auth, this.email, this.password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        console.log(user);
+        alert("成功");
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log("errorCode" + errorCode);
+        console.log("errorMessage" + errorMessage);
+      });
 
-      this.$router.push("/bookList");
-    } catch (error) {
-      console.log("エラー発生：" + error);
-      this.errorSignup = "登録に失敗しました";
-    }
+    // async signup(): Promise<void> {
+    //   try {
+    //     console.log("email:" + this.email);
+    //     console.log("pass:" + this.password);
+
+    //     const auth = getAuth(firebaseApp);
+    //     const user = createUserWithEmailAndPassword(
+    //       auth,
+    //       this.email,
+    //       this.password
+    //     );
+    //     console.log(user);
+
+    //     this.$router.push("/bookList");
+    //   } catch (error) {
+    //     console.log("エラー発生：" + error);
+    //     this.errorSignup = "登録に失敗しました";
+    //   }
+    // }
   }
 }
 </script>
