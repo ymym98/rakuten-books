db380d614af549731dd43905865f1104b7170fd8
ymym98 <jimboyumi@gmail.com>
2022-03-08 17:17:04 +0900

    コメントを追加しました

--- src/config/firebase-config.ts
+++ src/config/firebase-config.ts
@@ -11,9 +11,10 @@ const firebaseConfig = {
   databaseURL: process.env.VUE_APP_DATABASE_URL,
 };
 
+// 論理否定だから、firebase.apps.lengthがある時(true)はfalseの処理に。ない時(false)はtrueの処理にいく。
 export const firebaseApp = !firebase.apps.length
-  ? firebase.initializeApp(firebaseConfig)
-  : firebase.app();
+  ? firebase.initializeApp(firebaseConfig) // ← なかったら初期化
+  : firebase.app(); // ← あったらインスタンスを取得
 
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore();
