accf1832e546338df75904a81eea413ac760636f
ymym98 <jimboyumi@gmail.com>
2022-03-08 15:33:20 +0900

    不要なコードを削除しました　

--- src/config/firebase-config.ts
+++ src/config/firebase-config.ts
@@ -1,6 +1,5 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-// import firebase from "firebase/compat/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
@@ -10,10 +9,6 @@ const firebaseConfig = {
   storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
   databaseURL: process.env.VUE_APP_DATABASE_URL,
 };
-
-// export const firebaseApp = !firebase.apps.length
-//   ? firebase.initializeApp(firebaseConfig)
-//   : firebase.app();
 
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore();
